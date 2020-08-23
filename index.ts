84af1db6a2af30b4b35fe08b3a5790b5cfaf33ab
Anwesh43 <anweshthecool0@gmail.com>
2020-08-23 17:52:57 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -21,3 +21,31 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
  }
+
+ class DrawingUtil {
+    
+
+    static drawCircle(context : CanvasRenderingContext2D, x : number, y : number, r : number) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawBallRollCircle(context : CanvasRenderingContext2D, scale : number) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        const y : number = h - h * 0.5 * sf2 
+        const size : number = w / parts  
+        DrawingUtil.drawCircle(context, size / 2 + (w - size / 2) * sf4, y - size / 2, size * 0.5 * sf1)
+        context.fillRect(0, y, size, h * 0.5 * sf2)
+        context.fillRect(size, h * 0.5, (w - size) * sf3, h * 0.5)
+    }
+
+    static drawBRCNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        DrawingUtil.drawBallRollCircle(context, scale)
+    }
+ }
